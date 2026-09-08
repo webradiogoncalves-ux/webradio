e43c82132e09c3c4a428fb491546a4913f9f03b9
webradiogoncalves-ux <webradiogoncalves@gmail.com>
2026-09-08 18:36:10 -0300

    Add files via upload

---
+++ app/src/main/java/com/vemcomigo/radio/RadioConfig.java
@@ -0,0 +1,9 @@
+package com.vemcomigo.radio;
+
+public final class RadioConfig {
+    private RadioConfig() {}
+
+    // Transmissão direta da rádio.
+    public static final String RADIO_STREAM_URL =
+            "http://sapircast.caster.fm:19513/QKnuH";
+}
