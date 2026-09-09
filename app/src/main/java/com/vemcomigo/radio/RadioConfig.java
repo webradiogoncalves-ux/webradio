d1e1ff1c335d106fe7cd894ee44830c558047ddd
webradiogoncalves-ux <webradiogoncalves@gmail.com>
2026-09-08 23:59:46 -0300

    Delete app directory

--- app/src/main/java/com/vemcomigo/radio/RadioConfig.java
+++
@@ -1,9 +0,0 @@
-package com.vemcomigo.radio;
-
-public final class RadioConfig {
-    private RadioConfig() {}
-
-    // Transmissão direta da rádio.
-    public static final String RADIO_STREAM_URL =
-            "http://sapircast.caster.fm:19513/QKnuH";
-}
