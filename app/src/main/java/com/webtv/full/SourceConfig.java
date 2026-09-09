e6c013d96ae01aa63761a9b37ee2734acade1587
webradiogoncalves-ux <webradiogoncalves@gmail.com>
2026-09-09 12:35:02 -0300

    Add files via upload

---
+++ app/src/main/java/com/webtv/full/SourceConfig.java
@@ -0,0 +1,9 @@
+package com.webtv.full;
+
+/** Fontes-base configuradas. Os endereços não são exibidos na interface. */
+public final class SourceConfig {
+    private SourceConfig() {}
+    public static final String ESPORTES_SOURCE = "https://futemax.ke/";
+    public static final String NOVELAS_SOURCE = "https://noveflix.lol/";
+    public static final String CANAIS_SOURCE = "https://www17.redecanais.in/";
+}
